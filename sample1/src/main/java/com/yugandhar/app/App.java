9a1cf6bcae1a4f1568883f7114bb93f617465b32
yugandhar <yugandhar4466@gmail.com>
2022-03-14 10:10:56 +0000

    first commit

---
+++ sample1/src/main/java/com/yugandhar/app/App.java
@@ -0,0 +1,13 @@
+package com.yugandhar.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!,learning git" );
+    }
+}
