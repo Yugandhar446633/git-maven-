b1dfff807c1a4a40126fd52a97952d90def5a355
Yugandhar446633 <98577711+Yugandhar446633@users.noreply.github.com>
2022-03-18 11:00:55 +0530

    Update App.java

--- sample2/src/main/java/com/yugandhar/app/App.java
+++ sample2/src/main/java/com/yugandhar/app/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!,learning git" );
-        system.out.println( "new line added and new branch created ");
+        system.out.println( "new line added ");
     }
 }    
                                                                                                                             }    
