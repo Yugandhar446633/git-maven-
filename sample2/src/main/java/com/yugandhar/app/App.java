3e04870597e6bdf29f72b570c56ac78a169c51c8
Yugandhar446633 <98577711+Yugandhar446633@users.noreply.github.com>
2022-03-18 10:59:17 +0530

    changes done to new branch

--- sample2/src/main/java/com/yugandhar/app/App.java
+++ sample2/src/main/java/com/yugandhar/app/App.java
@@ -1,5 +1,4 @@
 package com.yugandhar.app;
-
 /**
  * Hello world!
  *
@@ -8,6 +7,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!,learning git " );
+        system.out.println( "new line added and new branch created ");
     }
-}
+}    
+    
