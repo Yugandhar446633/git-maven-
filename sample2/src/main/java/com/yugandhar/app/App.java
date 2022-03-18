ea26e7545d85fe859e3bfa3c0b48284f8065b776
yugandhar446633 <yugandhar4466@gmail.com>
2022-03-18 06:48:14 +0000

    changes done from uat4

--- sample2/src/main/java/com/yugandhar/app/App.java
+++ sample2/src/main/java/com/yugandhar/app/App.java
@@ -7,7 +7,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        system.out.println( "i am in feature-branch")
+        system.out.println( "i am in uat4 branch")
         System.out.println( "Hello World!,learning git " );
         system.out.println( "new line added and new branch created ");
     }
