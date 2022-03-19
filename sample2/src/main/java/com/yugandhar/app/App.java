9017ee217546c5ffb9199f4fdcfceaf99e85a8d9
yugandhar446633 <yugandhar4466@gmail.com>
2022-03-19 11:47:43 +0000

    amended changes

--- sample2/src/main/java/com/yugandhar/app/App.java
+++ sample2/src/main/java/com/yugandhar/app/App.java
@@ -1,4 +1,5 @@
 package com.yugandhar.app;
+
 /**
  * Hello world!
  *
@@ -7,18 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
-<<<<<<< HEAD
         System.out.println( "Hello World!,learning git" );
-        system.out.println( "new line added ");
+        system.out.println( "new line added and new branch created ");
     }
 }    
                                                                                                                             }    
    
-=======
-        system.out.println( "i am in uat4 branch")
-        System.out.println( "Hello World!,learning git " );
-        system.out.println( "new line added and new branch created ");
-    }
-}    
-    
->>>>>>> uat4
