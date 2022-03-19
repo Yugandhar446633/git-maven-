39224af70a428e5502353439771977ef78c05712
yugandhar446633 <yugandhar4466@gmail.com>
2022-03-19 11:22:53 +0000

    soft restet

--- sample2/src/main/java/com/yugandhar/app/App.java
+++ sample2/src/main/java/com/yugandhar/app/App.java
@@ -1,5 +1,4 @@
 package com.yugandhar.app;
-
 /**
  * Hello world!
  *
@@ -8,9 +7,18 @@ public class App
 {
     public static void main( String[] args )
     {
+<<<<<<< HEAD
         System.out.println( "Hello World!,learning git" );
-        system.out.println( "new line added and new branch created ");
+        system.out.println( "new line added ");
     }
 }    
                                                                                                                             }    
    
+=======
+        system.out.println( "i am in uat4 branch")
+        System.out.println( "Hello World!,learning git " );
+        system.out.println( "new line added and new branch created ");
+    }
+}    
+    
+>>>>>>> uat4
